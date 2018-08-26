045b3242c9cbc9d58de3057f697cefcc5089ad52
BondYuriy <bondar.y@i.ua>
2018-08-26 14:16:37 +0300

    adds fix hw-08

--- module-08/js/index.js
+++ module-08/js/index.js
@@ -9,48 +9,80 @@ const galleryItems = [
   { preview: 'img/preview-6.jpeg', fullview: 'img/fullview-6.jpeg', alt: "alt text 6" },
 ];
 
-const gallery = document.querySelector('.js-image-gallery');
+class Gallery {
+  constructor({ items, parentNode, defaultActiveItem }) {
+    this.items = items;
+    this.parentNode = parentNode;
+    this.defaultActiveItem = defaultActiveItem;
+    this.setActivePreviewItems(this.items[defaultActiveItem]);
+    this.list = document.querySelector(".preview");
+    this.list.addEventListener("click", this.onClick);
+  }
 
-const fullview = document.createElement('div');
-fullview.classList.add('fullview');
+  setActivePreviewItems() {
+    const fullviewImgSrc = this.items[this.defaultActiveItem].fullview;
+    const fullviewImgAlt = this.items[this.defaultActiveItem].alt;
+    const fullview = this.createFullview(fullviewImgSrc, fullviewImgAlt);
+    const preview = this.createPreview(this.items);
+    this.parentNode.append(fullview, preview);
+  }
 
-const fullviewImg = document.createElement('img');
-fullviewImg.setAttribute('src', galleryItems[0].fullview);
+  createFullview(src, alt) {
+    const fullview = document.createElement("div");
+    fullview.classList.add("fullview");
 
-const list = document.createElement('ul');
-list.classList.add('preview');
+    const fullviewImg = document.createElement("img");
+    fullviewImg.setAttribute("src", src);
+    fullviewImg.setAttribute("alt", alt);
+    fullviewImg.classList.add("js-fullview-img");
 
-const itemList = galleryItems.map(elem => {
-  const item = document.createElement('li');
-  const img = document.createElement('img');
-  img.classList.add('js-img');
-  img.setAttribute('src', elem.preview);
-  img.setAttribute('data-fullview', elem.fullview);
-  img.setAttribute('alt', elem.alt);
-  item.append(img);
-  return item;
-});
+    fullview.appendChild(fullviewImg);
+    return fullview;
+  }
 
-gallery.append(fullview, list);
-fullview.appendChild(fullviewImg);
-list.append(...itemList);
+  createPreview(items) {
+    const list = document.createElement("ul");
+    list.classList.add("preview");
 
-const img = document.querySelector('.js-img');
-img.classList.add('selected');
+    const itemList = items.map(item => {
+      const listItem = document.createElement("li");
+      const listItemImg = document.createElement("img");
+      listItemImg.classList.add("js-img");
+      listItemImg.setAttribute("src", item.preview);
+      listItemImg.setAttribute("data-fullview", item.fullview);
+      listItemImg.setAttribute("alt", item.alt);
+      listItem.append(listItemImg);
+      return listItem;
+    });
 
-const onClickImg = (event) => {
-  const target = event.target;
-  const nodeName = event.target.nodeName;
-  const imeges = document.querySelectorAll('.js-img');
-  if (nodeName === 'IMG') {
-    const fullviewImgNew = target.dataset.fullview;
-    fullviewImg.removeAttribute('src','');
-    fullviewImg.setAttribute('src', fullviewImgNew);
-    imeges.forEach(imege => {
-      imege.classList.remove('selected');
-    })
-    target.classList.add('selected');
+    list.append(...itemList);
+    return list;
+  }
+
+  onClick(evt) {
+    evt.preventDefault();
+    const target = event.target;
+    const nodeName = event.target.nodeName;
+
+    if (nodeName === "IMG") {
+      const fullviewImg = document.querySelector(".js-fullview-img");
+      const imeges = document.querySelectorAll(".js-img");
+
+      fullviewImg.removeAttribute("src", "");
+      fullviewImg.removeAttribute("alt", "");
+      fullviewImg.setAttribute("src", target.dataset.fullview);
+      fullviewImg.setAttribute("alt", target.alt);
+
+      imeges.forEach(imege => {
+        imege.classList.remove("selected");
+      });
+      target.classList.add("selected");
+    }
   }
 }
 
-list.addEventListener('click', onClickImg);
+const gallery = new Gallery({
+  items: galleryItems,
+  parentNode: document.querySelector(".image-gallery"),
+  defaultActiveItem: 0
+});
